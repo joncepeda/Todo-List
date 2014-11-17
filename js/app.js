520a411e7985f335952fbb7cc684d6e55b7c50c2
joncepeda <jon.jonathancepeda@gmail.com>
2014-11-17 20:57:43 +0800

    first todo

---
+++ js/app.js
@@ -0,0 +1,40 @@
+var todoApp = angular.module("todoApp",['ngRoute']);
+todoApp.config(['$routeProvider', function ($routeProvider){
+	$routeProvider
+		.when('/create',
+		{
+			controller: 'SimpleController',
+			templateUrl: 'create.html'
+		})
+		.when('/view',
+		{
+			controller: 'SimpleController',
+			templateUrl: 'view.html'
+		})
+		.when('/delete',{
+			controller: 'SimpleController',
+			templateUrl: 'delete.html'
+		})
+		.otherwise({ redirectTo : '/view.html'});
+}]);
+
+todoApp.controller('SimpleController',['$scope','$http',function ($scope,$http){
+		$http.get('js/dataRecords.json').success(function(data){
+			console.log(data);
+			$scope.todolist = data;
+			
+		})
+		.error(function(data,status){
+			console.log(status);
+		});
+
+		$scope.addList = function(){
+			$scope.list.push({
+				name:$scope.newList.name, 
+				start_date: $scope.newList.start_date,
+				end_date:$scope.newList.end_date
+			});
+			console.log("p");
+			location.href = 'view.html';
+		};
+}]);
